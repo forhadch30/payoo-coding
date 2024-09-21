24b17393db72518cd404e33a098df35f1a4ade71
forhad30 <forhadchariani30@gmail.com>
2024-09-21 15:51:06 +0600

    first commit payoo meney

---
+++ payoo.js
@@ -0,0 +1,11 @@
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault()
+    const numbers = document.getElementById('input-number').value;
+    const pin = document.getElementById('input-pin').value;
+    if (numbers === '5' && pin === '1234') {
+        console.log('great job');
+    }
+    else{
+        alert('unknown email')
+    }
+})
