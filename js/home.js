d107b8cc47708946f3ed103ac12e0c9f13ba3e6e
forhad30 <forhadchariani30@gmail.com>
2024-09-23 08:56:13 +0600

    home done

--- js/home.js
+++ js/home.js
@@ -1,24 +1,4 @@
-// document.getElementById('add-Money').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     const cashOutAmount = document.getElementById('cash-out-amount').value;
-//     const cashOutPins = document.getElementById('pin-pass').value;
-//     const currentAmount = document.getElementById('amount').innerText;
-
-
-//     // String to number 
-//     const cashOutTotal = parseFloat(cashOutAmount)
-//     const cashOutTotalAmount = parseFloat(currentAmount)
-
-//     if (cashOutPins === '1234') {
-
-//         const cashOutBalance = cashOutTotalAmount + cashOutTotal;
-//         const prev = document.getElementById('amount');
-//         prev.innerText = cashOutBalance;
-
-//     }
-// })
-
-document.getElementById('add-Money').addEventListener('click', function (event) {
+document.getElementById('add-money').addEventListener('click', function (event) {
         event.preventDefault();
         console.log('clicked');
 })
