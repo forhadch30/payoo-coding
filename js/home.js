594304f66d77f65766c5809aa6ad00bc2c419bb7
forhad30 <forhadchariani30@gmail.com>
2024-09-23 13:52:56 +0600

    second done

--- js/home.js
+++ js/home.js
@@ -1,4 +1,20 @@
 document.getElementById('add-money').addEventListener('click', function (event) {
-        event.preventDefault();
-        console.log('clicked');
+        event.preventDefault()
+
+        const amountIn = document.getElementById('amount-input').value;
+        const inputPin = document.getElementById('input-pin').value;
+        const currentValue = document.getElementById('current-amount').innerText;
+
+        // String to number 
+        const amountNumber = parseFloat(amountIn);
+        const currentNumber = parseFloat(currentValue)
+
+        if (inputPin === '1111') {
+                console.log('okay come here');
+                const amountTotal = currentNumber - amountNumber;
+                document.getElementById('current-amount').innerText = amountTotal;
+        }
+        else {
+                alert('Please try again')
+        }
 })
